580fa4121a7969935c59dd94a658df0fd5590d46
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-06-15 17:04:15 +0200

    deleted empty tag

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -13,25 +13,23 @@ export class ContactList extends Component {
     const { contacts } = this.props;
 
     return (
-      <>
-        <ul>
-          {contacts.map(contact => {
-            return (
-              <li key={contact.id} className={css.listEl}>
-                {contact.name}: {contact.number}
-                <button
-                  type="submit"
-                  value={contact.id}
-                  onClick={this.handleDelete}
-                  className={css.delBtn}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </>
+      <ul>
+        {contacts.map(contact => {
+          return (
+            <li key={contact.id} className={css.listEl}>
+              {contact.name}: {contact.number}
+              <button
+                type="submit"
+                value={contact.id}
+                onClick={this.handleDelete}
+                className={css.delBtn}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
